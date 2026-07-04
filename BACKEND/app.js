8081d2aa7e691cba0828a5e6143d7f1885e4e56f
ABHI <abhisekprusty012@gmail.com>
2026-07-04 15:34:08 +0530

    creating routes

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,8 +1,18 @@
 import express from "express";
+import { nanoid } from "nanoid";
+
+
 const app = express();
 
+
+app.get("/api/create",(req,res)=>{
+    res.send("Hello world");
+})
+
+
+
+
 app.listen(3000,()=>{
-    console.log("Server is running on port 5000");
+    console.log("Server is running on port http://localhost:3000");
     
 })
-console.log("Hi")
