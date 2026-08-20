6fbdd361f43a8c43c936be24c08326b2a65d9315
ABHI <abhisekprusty012@gmail.com>
2026-08-21 01:55:00 +0530

    shortened the url

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,24 +3,29 @@ import { nanoid } from "nanoid";
 import dotenv from "dotenv";
 dotenv.config("./.env");
 import connectDb from "./SRC/config/mongodb.config.js";
-
+import shortUrl from "./SRC/config/models/shorturl.model.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-app.post("/api/create",(req,res)=>{
-    const {url} = req.body
 
-    console.log(url);
-    res.send(nanoid(6));
-    
-    
+// 1. Get the original URL → 2. Generate a short ID → 3. Create a document → 4. Save it to MongoDBY
+app.post("/api/create",async(req,res)=>{
+    const {url} = req.body
+    const shortUrlId = nanoid(7)
+    const newUrl = new shortUrl({
+        full_url:url,
+        short_url: shortUrlId
+    })
+    await newUrl.save(); 
+    res.send(shortUrlId)
 })
+
 app.get("/api/get",(req,res)=>{
     console.log("get data");
-    
+    y
 })
 
 
--- notes.md
+++ notes.md
@@ -1,1 +1,5 @@
 connectDb insted of connectDB(yt) in the mongodb.config.js and app.js
+shorturl.model.js -> file name
+shortUrlSchema → Schema
+ShortUrl       → Mongoose Model which we exported and used in app.js
+shortUrlId     → generated NanoID
