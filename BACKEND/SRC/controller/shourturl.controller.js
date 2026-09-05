1b2cf207d225406ef08cc59a473f9e384fd95351
ABHI <abhisekprusty012@gmail.com>
2026-09-05 13:22:29 +0530

    Handle errors in the controller

--- BACKEND/SRC/controller/shourturl.controller.js
+++ BACKEND/SRC/controller/shourturl.controller.js
@@ -2,9 +2,15 @@ import { createshorturlWithoutUserservice } from "../services/shorturl.services.
 import { getShortlUrl } from "../dao/shorturl.js";
 
 export const createShourtUrl = async (req, res) => {
-  const { url } = req.body;
-  const shortUrl = await createshorturlWithoutUserservice(url);
-  res.send(process.env.APP_URL + shortUrl);
+  try {
+    const { url } = req.body;
+    const shortUrl = await createshorturlWithoutUserservice(url);
+    res.send(process.env.APP_URL + shortUrl);
+  }catch(error){
+    console.error(error);
+    res.status(500).send("Something went wrong.")
+
+  }
 };
 
 export const redirectfromshortUrl = async (req, res) => {
